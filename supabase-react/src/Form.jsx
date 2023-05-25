46aa629f8e55cbae316fef9546c7db87ab1b8e72
Peter Kress <PKress@mathematica-mpr.com>
2023-05-24 21:26:12 -0500

    expl-ip: initial attempt to pull text from album table

--- supabase-react/src/Form.jsx
+++ supabase-react/src/Form.jsx
@@ -1,14 +1,48 @@
 
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
 
-function App() {
+function Form() {
+    
+  const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState(null);
+  const [album, setAlbum] = useState(null);
+
+  
+  useEffect(() => {
+    async function getAlbum() {
+      setLoading(true);
+
+      let { data, error } = await supabase
+        .from('albums')
+        .select(`id, album_name`); //, artist_name, release_date, album_photo_id`)  
+
+      if (error) {
+        console.warn(error);
+      } else if (data) {
+        setAlbum(data.album_name);
+      }
+
+      setLoading(false);
+    }
+
+    getAlbum();
+  });
+
   return (
     <div className="wrapper">
-      <h1>How About Them Apples</h1>
+      <h1>Initial Form</h1>
       <form>
+      <fieldset>
+         <label>
+           <p>Album name: {album}</p>
+           <input review="review..." />
+         </label> 
+       </fieldset>
+       <button type="submit">Submit</button>
       </form>
     </div>
   )
 }
 
-export default App;
+export default Form;
